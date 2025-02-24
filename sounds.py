3fa3cbec1719248201293caf62c3a915dc60f091
JellkaGamez <jellkagamez14@gmail.com>
2025-02-23 19:11:57 -0500

    fixed some copying errors

--- sounds.py
+++ sounds.py
@@ -1,19 +1,18 @@
 import pygame
-import src
 
 def load_sound(name):
-    sound = src.pygame.mixer.Sound(name)
+    sound =     pygame.mixer.Sound(name)
     return sound
 
 def play_sound(sound):
-    src.pygame.mixer.Sound.play(sound)
+        pygame.mixer.Sound.play(sound)
 
 def set_music(name):
-    music = src.pygame.mixer.music.load(name)
+    music =     pygame.mixer.music.load(name)
     return music
 
 def play_music():
-    src.pygame.mixer.music.play()
+        pygame.mixer.music.play()
 
 def stop_all_music():
-    src.pygame.mixer.music.stop()
+        pygame.mixer.music.stop()
